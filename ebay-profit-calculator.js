74f90731c03c95e19d532cdc0d255fd80aeae5a9
jwrtn <johnnywirtanen@gmail.com>
2020-02-11 15:11:43 -0500

    Create ebay-profit-calculator.js

---
+++ ebay-profit-calculator.js
@@ -0,0 +1,56 @@
+// INPUT VARIABLES
+
+let ebaySalesPrice = 250
+let productBuyCost = 190
+let ebayShippingCost = 5
+let otherCosts = 0
+let totalWorkMinutes = 10
+let marketplaceFees = .09
+let paymentProcessingFees = .029
+let promotionFees = .036
+
+// FEE, COST, PROFIT & ROI CALCULATIONS
+
+let totalFees = marketplaceFees + paymentProcessingFees + promotionFees
+let revenueAfterFees = (ebaySalesPrice - (ebaySalesPrice * totalFees))
+let totalCost = productBuyCost + ebayShippingCost + otherCosts
+let netProfit = revenueAfterFees - totalCost
+let profitPerMinute = netProfit / totalWorkMinutes
+let profitPerHour = profitPerMinute * 60
+let profitPercent = (netProfit / ebaySalesPrice) * 100
+let netRoi =  (netProfit / totalCost) * 100
+
+// FEEDBACK TEMPLATE
+
+let feedbackTemplate = '$' + Math.round(profitPerMinute * 100) / 100 + ' profit per minute for ' + totalWorkMinutes + ' minutes of work ($' + Math.round(profitPerHour * 100) / 100 + '/hour) (' + Math.round(netRoi * 100) / 100 + '% net ROI)'
+
+// SHOW SALES PRICE, TOTAL COSTS, TOTAL FEES, NET PROFIT & GIVE FEEDBACK
+
+let giveFeedback = function () {
+    console.log('')
+    console.log('Sales price = $' + ebaySalesPrice)
+    console.log('Total costs = $' + Math.round(totalCost * 100) / 100 + ' ($' + productBuyCost + ' buy cost + $' + ebayShippingCost + ' ebay shipping cost + $' + otherCosts + ' other costs)')
+    console.log('Total fees = $' + Math.round(ebaySalesPrice * totalFees * 100) / 100 + ' (' + Math.round(marketplaceFees * 100) + '% marketplace + ' + Math.round(paymentProcessingFees * 100) + '% payment processing + ' + Math.round(promotionFees * 100) + '% promotions = ' + Math.round(totalFees * 100) + '% total)')
+    console.log('Net profit = $' + Math.round(netProfit * 100) / 100 + ' (' + Math.round(profitPercent * 100) / 100 + '%) ($' + revenueAfterFees + ' revenue after fees - $' + totalCost + ' total cost)')
+    console.log('')
+
+    if (profitPercent <= 5 || netRoi <= 5) {
+        console.log('No go... not enough profit!' + feedbackTemplate)
+    } else if (profitPercent >= 15 || netRoi >= 15) {
+        if (profitPerMinute >= .5) {
+        console.log('Excellent buy. ' + feedbackTemplate)
+        } else {
+            console.log('No go. Not enough profit for your time. ' + feedbackTemplate)
+        }
+    } else if (profitPercent >= 10 || netRoi >= 10) {
+        if (profitPerMinute >= .5) {
+        console.log('Good buy. ' + feedbackTemplate)
+        } else {
+            console.log('No go. Not enough profit for your time. ' + feedbackTemplate)
+        }
+    } else {
+        console.log('No go. Not enough profit. ' + feedbackTemplate)
+    }
+}
+
+giveFeedback()
